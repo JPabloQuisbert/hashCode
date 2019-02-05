7b0f0e2c8f167e77eab1742a4c59dfba7a88b8d6
Juan Pablo <jpabloquisbert@gmail.com>
2019-02-04 17:02:56 -0800

    Update lectura1.py

--- lectura1.py
+++ lectura1.py
@@ -7,5 +7,14 @@ Created on Sun Feb  3 20:16:40 2019
 import sys
 import numpy as np
 
+def analizar_data(file):
+    datos = []
+    with open(file, 'r') as f:
+        for i, line in enumerate(f):
+            if i == 0: rows, cols, ingredients, cells = line.split(' ')
+            else: 
+return datos, int(rows), int(cols), int(ingredients), int(cells)
+
 if __name__=='__main__':
+    
     
