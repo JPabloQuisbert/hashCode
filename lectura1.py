a89011c08982b67f37427782dd85e0259a3ccddd
Juan Pablo <jpabloquisbert@gmail.com>
2019-02-09 01:15:30 -0800

    Update lectura1.py

--- lectura1.py
+++ lectura1.py
@@ -14,6 +14,17 @@ def analizar_data(file):
             if i == 0: rows, cols, min_ing, max_cell = line.split(' ')
     return int(rows), int(cols), int(min_ing), int(max_cell)
 
+def leer_pizza(filename):
+    with open(filename,'r') as f:
+        line=f.readline()
+        R,C,L,H=line.split()
+        pizza=f.readlines()
+    return R,C,L,H,pizza
+
 if __name__=='__main__':
     b=analizar_data("data/a_example.in")
     print(b)
+    R,C,L,H,pizza=leer_pizza("data/a_example.in")
+    print(R,C,L,H)
+    print(pizza)
+    
