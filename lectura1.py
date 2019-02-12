a61840b5828c24cadc810dcc0f48b52f85f7ebdf
Juan Pablo <jpabloquisbert@gmail.com>
2019-02-11 21:44:25 -0800

    Update lectura1.py

--- lectura1.py
+++ lectura1.py
@@ -16,6 +16,8 @@ def analizar_data(file):
             if i == 0: rows, cols, min_ing, max_cell = line.split(' ')
     return int(rows), int(cols), int(min_ing), int(max_cell)
 
+
+#leer el archivo
 def leer_pizza(filename):
     with open(filename,'r') as f:
         line=f.readline()
@@ -24,13 +26,23 @@ def leer_pizza(filename):
         pi = np.array([list(map(lambda item: 1 if item == 'T' else 0, row.strip())) for row in pizza[0:]])
     return R,C,L,H,pizza,pi
 
+#contar los ingredientes
+def contar_ingredientes(array):
+    count_ing=[0,0]
+    for line in array:
+        count_ing+=np.bincount(line)
+    return count_ing
+
+
 if __name__=='__main__':
     b=analizar_data("data/a_example.in")
     print(b)
-    R,C,L,H,pizza,pi=leer_pizza("data/a_example.in")
+    R,C,L,H,pizza,pi=leer_pizza("data/b_small.in")
     print(R,C,L,H)
     print(pizza)
     print(pi)
     fig,ax=plt.subplots()
     ax.imshow(pi)
     ax.set_title(f'a_example.in shape is {pi.shape}, max. score {pi.size}\nmin. ingredients is {L}, max. pizza slice is {H}');
+    T,M=contar_ingredientes(pi)
+    print("tomates= ",T,"mushroom= ",M)
